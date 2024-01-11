9e7973fbb6338fb8bfc6b0fd7832015c62ec6297
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-11 17:54:48 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
-// App.js
-import './App.css';
-import Helloword from './components/Helloword';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import List from './components/List'
-import Evento from './components/Eventos';
-import Form from './components/form';
-import Condicional from './components/condicional';
-import OutraLista from './components/OutraLista';
-import Olá from './components/seuNome';
-import Saudacao from './components/Saudacao';
-import { useState } from 'react';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
-
-  const [nome,setNome] = useState()
-
-  const meusItens = ['react','vue','angular'] 
-
-  const url = "https://via.placeholder.com/150"
   return (
-    <div className="App">
-      <h1>testando css</h1>
-      <Frase/>
-      <Helloword />
-      <img src={url}></img>
-      <SayMyName nome="joão" />
-      <Pessoa
-      nome = 'artur'
-      idade = '17'
-      profissao = 'programador front-end'
-      foto="https://via.placeholder.com/150"
-      />
-      <List/>
-      <Evento/>
-      <Form/>
-      <Condicional/>
-      <OutraLista itens={meusItens}/>
-      <Olá setNome={setNome}/>
-      {nome}
-      <Saudacao nome={nome}/>
-    </div>
+    <Router>
+       <ul>
+         <li>Home</li>
+         <li>Contato</li>
+       </ul>
+    </Router>
   );
 }
+
 export default App;
